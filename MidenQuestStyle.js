99525d5ec218cfd2d989ba5f166d03e4db6f4b34
Christopher Jones <chris.jones@level-b.co.uk>
2016-05-12 20:18:38 +0100

    Changed file type

---
+++ MidenQuestStyle.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         MidenQuestStyle
+// @namespace    https://github.com/Vibblez/MidenQuestStyle
+// @version      0.1
+// @description  MidenQuest Enhancement Script
+// @updateURL    https://raw.githubusercontent.com/Vibblez/MidenQuestStyles/master/MidenQuestPlus.alpha.user.js
+// @author       Vibblez
+// @include      http://www.midenquest.com/Game.aspx
+// @include      http://midenquest.com/Game.aspx
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
