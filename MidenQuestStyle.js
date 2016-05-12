2a968459d717983b30af1bdfa39d84ce8cf56ba1
Christopher Jones <chris.jones@level-b.co.uk>
2016-05-12 20:23:05 +0100

    Added function to start applying styles

--- MidenQuestStyle.js
+++ MidenQuestStyle.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         MidenQuestStyle
 // @namespace    https://github.com/Vibblez/MidenQuestStyle
-// @version      0.1
+// @version      0.2
 // @description  MidenQuest Enhancement Script
-// @updateURL    https://raw.githubusercontent.com/Vibblez/MidenQuestStyles/master/MidenQuestPlus.alpha.user.js
+// @updateURL    https://raw.githubusercontent.com/Vibblez/MidenQuest/master/MidenQuestStyle.js
 // @author       Vibblez
 // @include      http://www.midenquest.com/Game.aspx
 // @include      http://midenquest.com/Game.aspx
@@ -11,4 +11,16 @@
 
 (function() {
     'use strict';
+
+    function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+    }
+
+    addGlobalStyle('.body { background-color: #fff !important;  }');
 })();
