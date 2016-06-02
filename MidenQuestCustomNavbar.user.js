537e29c9ac31a30b56a7ffd47920193fbab89781
Christopher Jones <chris.jones@level-b.co.uk>
2016-06-02 09:18:37 +0100

    Updated header information to allow updating correctly

--- MidenQuestCustomNavbar.user.js
+++ MidenQuestCustomNavbar.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         MidenQuestCustomNavbar
-// @namespace    https://github.com/Vibblez/MidenQuestStyle
-// @version      0.1.1
+// @namespace    https://github.com/Vibblez/MidenQuest
+// @version      0.1.2
 // @description  MidenQuest Enhancement Script
-// @updateURL    https://raw.githubusercontent.com/Vibblez/MidenQuest/master/MidenQuestCustomNavbar.js
+// @updateURL    https://raw.githubusercontent.com/Vibblez/MidenQuest/master/MidenQuestCustomNavbar.user.js
 // @author       Vibblez
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @include      http://www.midenquest.com/Game.aspx
